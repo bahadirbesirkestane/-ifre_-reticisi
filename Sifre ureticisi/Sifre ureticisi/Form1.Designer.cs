65558a8c6e97e01f74f3790932e7654c0ee2d9d5
bahadirbesirkestane <63306536+bahadirbesirkestane@users.noreply.github.com>
2023-02-28 01:08:04 +0300

    Add files via upload

---
+++ Sifre ureticisi/Sifre ureticisi/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace Sifre_ureticisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.numSifreKarakter = new System.Windows.Forms.NumericUpDown();
+            this.cmbBuyukHarf = new System.Windows.Forms.ComboBox();
+            this.txtHarfTercih = new System.Windows.Forms.TextBox();
+            this.btnOlustur = new System.Windows.Forms.Button();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.btnKopyala = new System.Windows.Forms.Button();
+            this.cmbTip = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numSifreKarakter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(293, 12);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(261, 24);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Şifre Karakter Sayısı ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(14, 12);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(261, 24);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.Text = "Şifre Tipleri";
+            // 
+            // numSifreKarakter
+            // 
+            this.numSifreKarakter.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numSifreKarakter.Location = new System.Drawing.Point(293, 69);
+            this.numSifreKarakter.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.numSifreKarakter.Name = "numSifreKarakter";
+            this.numSifreKarakter.Size = new System.Drawing.Size(261, 24);
+            this.numSifreKarakter.TabIndex = 2;
+            // 
+            // cmbBuyukHarf
+            // 
+            this.cmbBuyukHarf.Enabled = false;
+            this.cmbBuyukHarf.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbBuyukHarf.FormattingEnabled = true;
+            this.cmbBuyukHarf.Items.AddRange(new object[] {
+            "Harflerin Hepsi Küçük",
+            "Harflerin Hepsi Büyük",
+            "Sadece Baştaki Harf Büyük",
+            "Sadece Sondaki Harf Büyük",
+            "Büyük/Küçük Harf Karışık"});
+            this.cmbBuyukHarf.Location = new System.Drawing.Point(13, 177);
+            this.cmbBuyukHarf.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cmbBuyukHarf.Name = "cmbBuyukHarf";
+            this.cmbBuyukHarf.Size = new System.Drawing.Size(261, 26);
+            this.cmbBuyukHarf.TabIndex = 4;
+            this.cmbBuyukHarf.SelectedIndexChanged += new System.EventHandler(this.cmbBuyukHarf_SelectedIndexChanged);
+            // 
+            // txtHarfTercih
+            // 
+            this.txtHarfTercih.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtHarfTercih.Enabled = false;
+            this.txtHarfTercih.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtHarfTercih.Location = new System.Drawing.Point(13, 119);
+            this.txtHarfTercih.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtHarfTercih.Name = "txtHarfTercih";
+            this.txtHarfTercih.Size = new System.Drawing.Size(261, 24);
+            this.txtHarfTercih.TabIndex = 0;
+            this.txtHarfTercih.Text = "Büyük/Küçük Harf Tercihleri";
+            // 
+            // btnOlustur
+            // 
+            this.btnOlustur.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOlustur.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnOlustur.Location = new System.Drawing.Point(325, 131);
+            this.btnOlustur.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnOlustur.Name = "btnOlustur";
+            this.btnOlustur.Size = new System.Drawing.Size(191, 62);
+            this.btnOlustur.TabIndex = 5;
+            this.btnOlustur.Text = "OLUŞTUR";
+            this.btnOlustur.UseVisualStyleBackColor = true;
+            this.btnOlustur.Click += new System.EventHandler(this.btnOlustur_Click);
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSifre.Location = new System.Drawing.Point(13, 288);
+            this.txtSifre.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(721, 31);
+            this.txtSifre.TabIndex = 6;
+            // 
+            // btnKopyala
+            // 
+            this.btnKopyala.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKopyala.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnKopyala.Location = new System.Drawing.Point(543, 131);
+            this.btnKopyala.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnKopyala.Name = "btnKopyala";
+            this.btnKopyala.Size = new System.Drawing.Size(191, 62);
+            this.btnKopyala.TabIndex = 7;
+            this.btnKopyala.Text = "Kopyala";
+            this.btnKopyala.UseVisualStyleBackColor = true;
+            this.btnKopyala.Click += new System.EventHandler(this.btnKopyala_Click);
+            // 
+            // cmbTip
+            // 
+            this.cmbTip.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbTip.FormattingEnabled = true;
+            this.cmbTip.Items.AddRange(new object[] {
+            "Harf ve Sayı",
+            "Harf ve Özel Karakter",
+            "Sayı ve Özel Karakter",
+            "Harf, Sayı ve Özel Karakter",
+            "Sadece Sayı",
+            "Sadece Harf",
+            "Sadece Özel Karakter"});
+            this.cmbTip.Location = new System.Drawing.Point(16, 69);
+            this.cmbTip.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cmbTip.Name = "cmbTip";
+            this.cmbTip.Size = new System.Drawing.Size(260, 26);
+            this.cmbTip.TabIndex = 8;
+            this.cmbTip.SelectedIndexChanged += new System.EventHandler(this.cmbTip_SelectedIndexChanged);
+            this.cmbTip.TextChanged += new System.EventHandler(this.cmbTip_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(771, 397);
+            this.Controls.Add(this.cmbTip);
+            this.Controls.Add(this.btnKopyala);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.btnOlustur);
+            this.Controls.Add(this.cmbBuyukHarf);
+            this.Controls.Add(this.numSifreKarakter);
+            this.Controls.Add(this.txtHarfTercih);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Şifre Üreticisi";
+            ((System.ComponentModel.ISupportInitialize)(this.numSifreKarakter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.NumericUpDown numSifreKarakter;
+        private System.Windows.Forms.ComboBox cmbBuyukHarf;
+        private System.Windows.Forms.TextBox txtHarfTercih;
+        private System.Windows.Forms.Button btnOlustur;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Button btnKopyala;
+        private System.Windows.Forms.ComboBox cmbTip;
+    }
+}
+
---
+++ Sifre ureticisi/Sifre ureticisi/Form1.cs
@@ -0,0 +1,452 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sifre_ureticisi
+{
+    
+
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        string harf(int harfNumara, string sifre, int karakter, int i,Random random)
+        {
+           
+
+            int x;
+            char c;
+
+
+
+            // harf
+            if (harfNumara == 0)
+            {
+                x = random.Next(97, 123);
+                c = (char)x;
+
+                sifre += c.ToString();
+                //txtSifre.Text = c.ToString();
+
+
+            }
+            else if (harfNumara == 1)
+            {
+                x = random.Next(65, 91);
+                c = (char)x;
+
+                sifre += c.ToString();
+            }
+            else if (harfNumara == 2)
+            {
+                if(i==0)
+                {
+                    x = random.Next(65, 91);
+                    c = (char)x;
+
+                }
+                else
+                {
+                    x = random.Next(97, 123);
+                    c = (char)x;
+                }
+
+                sifre += c.ToString();
+
+               
+            }
+            else if (harfNumara == 3)
+            {
+
+
+                if (i == karakter - 1)
+                {
+                    x = random.Next(65, 91);
+                    c = (char)x;
+
+                    sifre += c.ToString();
+                }
+                else
+                {
+                    x = random.Next(97, 123);
+                    c = (char)x;
+
+                    sifre += c.ToString();
+                }
+
+            }
+            else //if (harfNumara == 4)
+            {
+                int zek = random.Next(0, 2);
+
+                if (zek == 0)  //büyük
+                {
+                    x = random.Next(65, 91);
+                    c = (char)x;
+
+                    sifre += c.ToString();
+
+                }
+                else   //küçük
+                {
+                    x = random.Next(97, 123);
+                    c = (char)x;
+
+                    sifre += c.ToString();
+                }
+            }
+
+            return sifre;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbTip_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbTip.Text == "Harf ve Sayı"|| cmbTip.Text == "Harf, Sayı ve Özel Karakter"|| cmbTip.Text== "Sadece Harf"|| cmbTip.Text== "Harf ve Özel Karakter")
+            {
+                txtHarfTercih.Enabled = true;
+                cmbBuyukHarf.Enabled = true;
+            }
+            else
+            {
+                txtHarfTercih.Enabled = false;
+                cmbBuyukHarf.Enabled = false;
+            }
+
+           
+        }
+
+        private void btnOlustur_Click(object sender, EventArgs e)
+        {
+            /*
+            Harf ve Sayı
+            Harf ve Özel Karakter
+            Sayı ve Özel Karakter
+            Harf, Sayı ve Özel Karakter
+            Sadece Sayı
+            Sadece Harf
+            Sadece Özel Karakter
+            */
+
+            /*
+             *  Harflerin Hepsi Küçük
+                Harflerin Hepsi Büyük
+                Sadece Baştaki Harf Büyük
+                Sadece Sondaki Harf Büyük
+                Büyük/Küçük Harf Karışık
+             */
+
+            
+            int karakter = Convert.ToInt32(numSifreKarakter.Text);
+
+            if(karakter==0)
+            {
+                MessageBox.Show("Lütfen Şifre Karakter Sayısını Giriniz", "Şifre Üreticisi");
+            }
+            
+            int harfNumara;
+
+            if(cmbBuyukHarf.Text== "Harflerin Hepsi Küçük")
+            {
+                harfNumara = 0;
+            }
+            else if(cmbBuyukHarf.Text == "Harflerin Hepsi Büyük")
+            {
+                harfNumara = 1;
+            }
+            else if (cmbBuyukHarf.Text == "Sadece Baştaki Harf Büyük")
+            {
+                harfNumara = 2;
+            }
+            else if (cmbBuyukHarf.Text == "Sadece Sondaki Harf Büyük")
+            {
+                harfNumara = 3;
+            }
+            else //(cmbBuyukHarf.Text == "Büyük/Küçük Harf Karışık")
+            {
+                harfNumara = 4;
+            }
+
+
+            
+            Random random = new Random();
+            Random random1=new Random();
+            Random random3 = new Random();
+            Random rastgele=new Random();
+
+            string sifre="";
+
+            int[] dizi = { 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 58, 59, 60, 61, 62, 63, 64, 91, 92, 93, 94, 95, 123, 124, 125, 126 };
+
+
+            if (cmbBuyukHarf.Enabled == true && cmbBuyukHarf.Text.Length == 0)
+            {
+                MessageBox.Show("Lütfen Büyük/Küçük Harf Tercihi Yapınız", "Şifre Üreticisi");
+            }
+            else
+            {
+                if (cmbTip.Text == "Harf ve Sayı")
+                {
+
+
+                    for (int i = 0; i < karakter; i++)
+                    {
+                        int z = rastgele.Next(0, 2);
+
+                        if (i == 0 && harfNumara == 2)
+                        {
+                            z = 0;
+                        }
+
+                        if (i == karakter - 1 && harfNumara == 3)
+                        {
+
+                            z = 0;
+                        }
+
+                        if (z == 0)
+                        {
+
+
+                            sifre = harf(harfNumara, sifre, karakter, i, random);
+
+
+                        }
+                        else
+                        {
+                            // sayi
+                            int y = random1.Next(0, 10);
+
+                            sifre += y.ToString();
+                        }
+
+
+
+                    }
+
+
+                    txtSifre.Text = sifre;
+
+                }
+                else if (cmbTip.Text == "Harf ve Özel Karakter")
+                {
+
+                    char ch;
+                    for (int i = 0; i < karakter; i++)
+                    {
+                        int z = rastgele.Next(0, 2);
+
+                        if (i == 0 && harfNumara == 2)
+                        {
+                            z = 0;
+                        }
+
+                        if (i == karakter - 1 && harfNumara == 3)
+                        {
+
+                            z = 0;
+                        }
+
+                        if (z == 0)
+                        {
+                            sifre = harf(harfNumara, sifre, karakter, i, random);
+
+                        }
+                        else  // özel karakter
+                        {
+                            int rast1 = random3.Next(0, 31);
+
+                            ch = (char)dizi[rast1];
+
+                            sifre += ch.ToString();
+                        }
+
+                    }
+
+
+                    txtSifre.Text = sifre;
+
+                }
+                else if (cmbTip.Text == "Sayı ve Özel Karakter")
+                {
+                    char c;
+
+                    for (int i = 0; i < karakter; i++)
+                    {
+                        int z = rastgele.Next(0, 2);
+
+                        if (z == 0)  // sayı
+                        {
+                            int y = random1.Next(0, 10);
+
+                            sifre += y.ToString();
+                        }
+                        else  // özel karakter
+                        {
+                            int rast1 = random3.Next(0, 31);
+
+                            c = (char)dizi[rast1];
+
+                            sifre += c.ToString();
+                        }
+
+
+
+                    }
+
+                    txtSifre.Text = sifre;
+                }
+                else if (cmbTip.Text == "Harf, Sayı ve Özel Karakter")
+                {
+                    char c;
+
+                    for (int i = 0; i < karakter; i++)
+                    {
+                        int z = rastgele.Next(0, 3);
+
+                        if (i == 0 && harfNumara == 2)
+                        {
+                            z = 0;
+                        }
+
+                        if (i == karakter - 1 && harfNumara == 3)
+                        {
+
+                            z = 0;
+                        }
+
+
+                        if (z == 0)  // harf
+                        {
+
+                            sifre = harf(harfNumara, sifre, karakter, i, random);
+
+
+                        }
+                        else if (z == 1) // özel karakter
+                        {
+                            int rast1 = random3.Next(0, 31);
+
+                            c = (char)dizi[rast1];
+
+                            sifre += c.ToString();
+                        }
+                        else  // sayı
+                        {
+                            int y = random1.Next(0, 10);
+
+                            sifre += y.ToString();
+                        }
+
+
+
+                    }
+
+                    txtSifre.Text = sifre;
+                }
+                else if (cmbTip.Text == "Sadece Sayı")
+                {
+                    for (int i = 0; i < karakter; i++)
+                    {
+                        int y = random1.Next(0, 10);
+
+                        sifre += y.ToString();
+                    }
+
+                    txtSifre.Text = sifre;
+                }
+                else if (cmbTip.Text == "Sadece Harf")
+                {
+                    for (int i = 0; i < karakter; i++)
+                    {
+                        sifre = harf(harfNumara, sifre, karakter, i, random);
+                    }
+
+                    txtSifre.Text = sifre;
+                }
+                else if (cmbTip.Text == "Sadece Özel Karakter")
+                {
+
+
+                    char c;
+
+
+
+
+                    for (int i = 0; i < karakter; i++)
+                    {
+                        int rast1 = random3.Next(0, 31);
+
+                        c = (char)dizi[rast1];
+
+                        sifre += c.ToString();
+
+
+                    }
+
+                    txtSifre.Text = sifre;
+                }
+
+            }
+
+
+
+
+
+        }
+
+        private void cmbTip_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnKopyala_Click(object sender, EventArgs e)
+        {
+            if(txtSifre.Text.Length > 0)
+            {
+                Clipboard.SetText(txtSifre.Text);
+            }
+
+
+            
+        }
+
+        private void cmbBuyukHarf_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /*
+            if(cmbBuyukHarf.Text.Length==0)
+            {
+                
+            }
+
+            */
+        }
+
+        private void btnBanaBirak_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void lstSifreler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //lstSifreler
+        }
+    }
+}
